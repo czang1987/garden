75194414c4d04b11d33e934c68ff797b5d327369
unknown <chenpeng.zang@outlook.com>
2026-02-13 18:38:33 -0500

    plants support category now

--- src/type/plants.ts
+++ src/type/plants.ts
@@ -1,5 +1,21 @@
-export type PlantMeta = {
+export type Footprint = [number, number]; // [h, w]
+
+export type PlantVariant = {
+  id: string;
   name: string;
   icon: string;
   baseHeight: number;
+  footprint?: Footprint;
+  tags?: string[];
+};
+
+export type PlantCategory = {
+  id: string;
+  name: string;
+  icon?: string;
+  variants: PlantVariant[];
+};
+
+export type PlantCatalogData = {
+  categories: PlantCategory[];
 };
